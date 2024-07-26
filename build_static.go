78b5b9844f21cb028db695e4a198ae8b965bdcb1
guoxi.wang <guoxi.wang@weiheng-tech.com>
2024-07-26 16:18:44 +0800

    fix: build glibc static

--- build_static.go
+++ build_static.go
@@ -21,4 +21,5 @@ package openssl
 // #cgo darwin CFLAGS: -I/usr/local/opt/openssl@1.1/include -I/usr/local/opt/openssl/include -Wno-deprecated-declarations
 // #cgo darwin LDFLAGS: -L/usr/local/opt/openssl@1.1/lib -L/usr/local/opt/openssl/lib -lssl -lcrypto
 // #cgo windows CFLAGS: -DWIN32_LEAN_AND_MEAN
+// #cgo LDFLAGS: -static-libgcc -static-libstdc++
 import "C"
